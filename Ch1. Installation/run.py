2bb1e9407c8217066e5d552a17b8d8110728bb81
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2022-02-06 22:24:32 +0100

    Update run.py

--- Ch1. Installation/run.py
+++ Ch1. Installation/run.py
@@ -4,15 +4,18 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home page</h1>"
+    return "<h1>Home page</h1>" \
+           "<p>In this project we develop and app...</p>"
 
 @app.route('/developers')
 def developers():
-    return "<h1>Developers</h1>"
+    return "<h1>Developers</h1>" \
+           "<p>In this webpage, we introduce the developers of the app...</p>"
 
 @app.route('/app_calculator')
 def app_calculator():
-    return "<h1>App Calculator</h1>"
+    return "<h1>App Calculator</h1>" \
+           "<p>In this webpage, we develop an app to work out transport carbon emissions...</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
